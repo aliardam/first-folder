3a8b24534aac6495a7525cef9c902b287247f603
Ali <aliardam402@gmail.com>
2024-04-13 15:02:58 +0200

    step 1

---
+++ inh.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ipv4address {
+    unsigned char a,b,c,d;
+    vector <unsigned char> unchar {a,b,c,d};
+    public:
+    
+
+};
