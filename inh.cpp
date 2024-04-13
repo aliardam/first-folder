3d78cf8da0e582f09fe15a315f15ac28eb497a69
Ali <aliardam402@gmail.com>
2024-04-13 17:02:19 +0200

    fix

--- inh.cpp
+++ inh.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 class ipv4address {
-    unsigned char a,b,c,d;
-    vector <unsigned char> unchar {a,b,c,d};
+    vector <unsigned char> address;
     public:
-    
+    ipv4address() : address(4) {}
 
+    unsigned char getad(int i) const {
+        if (i<1 || i>4){ cout<<"address does not exist"<<endl; return 0; }
+        return address[i];
+    }
+    void setad(int i,unsigned char n ) {
+        address[i]=n;
+    }
 };
