65eae8da438d0f9941b71f036ac8b426f4f07b52
Ali <aliardam402@gmail.com>
2024-04-14 03:39:27 +0200

    yeeeuuu

--- inh.cpp
+++ inh.cpp
@@ -6,12 +6,27 @@ class ipv4address {
     vector <unsigned char> address;
     public:
     ipv4address() : address(4) {}
+    ipv4address(unsigned char a, unsigned char b, unsigned char c, unsigned char d) : address({a, b, c, d}) {}
 
     unsigned char getad(int i) const {
         if (i<1 || i>4){ cout<<"address does not exist"<<endl; return 0; }
-        return address[i];
+        return address[i-1];
     }
     void setad(int i,unsigned char n ) {
         address[i]=n;
     }
+};
+
+class request {
+    ipv4address origin;
+    public:
+    request(ipv4address address) : origin(address) {}
+    ipv4address getorigin() const {
+        return origin;
+    }
+    void log () {
+        cout<<"Request of unknown type from"<<origin.getad(1)<<","<<origin.getad(2)<<","<<origin.getad(3)<<origin.getad(4);
+    }
+
+
 };
