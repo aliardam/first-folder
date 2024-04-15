8aaa502b3b9ebef36c4d301e174249a3673db1ab
Ali <aliardam402@gmail.com>
2024-04-15 17:57:41 +0200

    more additions

--- inh.cpp
+++ inh.cpp
@@ -21,12 +21,32 @@ class request {
     ipv4address origin;
     public:
     request(ipv4address address) : origin(address) {}
-    ipv4address getorigin() const {
+    ipv4address getorigincon() const {
         return origin;
     }
     void log () {
         cout<<"Request of unknown type from"<<origin.getad(1)<<","<<origin.getad(2)<<","<<origin.getad(3)<<origin.getad(4);
     }
+    protected:
+    ipv4address getorigin(){
+        return origin;
+    }
 
+
+};
+
+class HttpRequest : public request {
+    string url;
+    public:
+    HttpRequest(ipv4address address , string _url) : request(address) , url(_url) {}
+    void seturl(string _url){
+        url = _url;
+    }
+    string geturl() const {
+        return url;
+    }
+    void log () {
+        cout<<"Request of unknown type from"<<getorigin().getad(1)<<","<<getorigin().getad(2)<<","<<getorigin().getad(3)<<getorigin().getad(4)<<"to"<<geturl()<<endl;
+    }
 
 };
